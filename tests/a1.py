aeb46bb0b06067035b6ff20ef2122151291ae781
Thiago M. Pinto <tpinto@indeed.com>
2025-03-03 18:22:20 +0000

    remove unused modules and files

--- tests/a1.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-
-try:
-    task = asyncio.get_current_task()
-    print("asyncio.get_current_task() is available")
-except AttributeError:
-    print("asyncio.get_current_task() is NOT available")
-
-print(f"asyncio version: {asyncio.__version__}")
